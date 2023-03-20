5a83514da592bdbc2d8d015f457ffe5f7d5c42ec
AfredHussain-T <afredhussain69@gmail.com>
2023-03-20 15:00:16 +0530

    Uploaded

---
+++ foodparadise/src/Screens/Signin.js
@@ -0,0 +1,59 @@
+import React,{useState} from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+
+export default function Signin() {
+    let navigate=useNavigate();
+    const [credentials, setcreds] = useState({name:"",email:"",password:"",location:""})
+    const handleSubmit= async(e)=>{
+        e.preventDefault();
+        const response= await fetch("http://localhost:5000/api/createuser",{
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({name:credentials.name,email:credentials.email,password:credentials.password,location:credentials.location})
+            
+        });
+        
+        const json=await response.json()
+        
+        if(!json.success){
+            alert("Enter Valid Credtials")
+        }
+        if(json.success){
+            navigate("/");
+        }
+    }
+    const updatedCreds= (e)=>{
+        setcreds({...credentials,[e.target.name]:e.target.value})
+    }
+
+    return (
+        <>
+            <div className='container'>
+                <form onSubmit={handleSubmit}>
+                    <div className="mb-3">
+                        <label htmlFor="name" className="form-label" >Name</label>
+                        <input type="text" className="form-control" name='name' value={credentials.name} onChange={updatedCreds} />
+                        <div id="emailHelp" className="form-text">Enter your credentials correctly.</div>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="exampleInputEmail1" className="form-label" >Email address</label>
+                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name='email' value={credentials.email} onChange={updatedCreds} />
+                        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="exampleInputPassword1" className="form-label" >Password</label>
+                        <input type="password" className="form-control" id="exampleInputPassword1" name='password' value={credentials.password} onChange={updatedCreds}/>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="exampleInputAddress" className="form-label" >Current Address</label>
+                        <input type="text" className="form-control" name='location' value={credentials.location} onChange={updatedCreds}/>
+                    </div>
+                    <button type="submit" className="m-3 btn btn-primary">Submit</button>
+                    <Link to="/login" className="m-3 btn btn-warning">Existing User</Link>
+                </form>
+            </div>
+        </>
+    )
+}
